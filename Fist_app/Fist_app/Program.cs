9259cab344ab71c68a3fe117763c4a5261f7401f
JRZR1723 <zajo.deox@gmail.com>
2021-06-16 11:13:57 -0500

    Inicio_app

---
+++ Fist_app/Fist_app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fist_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
