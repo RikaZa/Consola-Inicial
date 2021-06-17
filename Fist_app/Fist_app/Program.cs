0c21a3eb6d2c9504d504f383d7eda4a53259afcb
JRZR1723 <zajo.deox@gmail.com>
2021-06-16 20:06:41 -0500

    clase 16-06-2021

--- Fist_app/Fist_app/Program.cs
+++ Fist_app/Fist_app/Program.cs
@@ -6,8 +6,15 @@ namespace Fist_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
-            Console.ReadKey();
+            if (args != null && args.Length > 0)
+            {
+                Console.WriteLine($"Hay {args.Length}  argumentos");
+                Console.WriteLine($"Tu nombre es: { args[0]}");
+                if (args.Length > 1)
+                    Console.WriteLine($"Tu apellido es: { args[1]}");
+            }else
+                Console.WriteLine("Hola desconocido");
+                Console.Read();
         }
     }
 }
