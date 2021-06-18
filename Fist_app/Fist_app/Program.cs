fba4b46edbe8d8f0c8563459b971f1362ab2de22
JRZR1723 <zajo.deox@gmail.com>
2021-06-18 01:02:36 -0500

    18/6/2021

--- Fist_app/Fist_app/Program.cs
+++ Fist_app/Fist_app/Program.cs
@@ -6,15 +6,51 @@ namespace Fist_app
     {
         static void Main(string[] args)
         {
-            if (args != null && args.Length > 0)
-            {
-                Console.WriteLine($"Hay {args.Length}  argumentos");
-                Console.WriteLine($"Tu nombre es: { args[0]}");
-                if (args.Length > 1)
-                    Console.WriteLine($"Tu apellido es: { args[1]}");
-            }else
-                Console.WriteLine("Hola desconocido");
-                Console.Read();
+            Console.WriteLine("Ingresa tu primer nombre:");
+            var name1 = Console.ReadLine();
+            Console.WriteLine("Ingresa tu segundo nombre:");
+            var name2 = Console.ReadLine();
+            Console.WriteLine("Ingresa tu primer apellido:");
+            var name3 = Console.ReadLine();
+            Console.WriteLine("Ingresa tu segundo apellido:");
+            var name4 = Console.ReadLine();
+            Console.WriteLine("Ingresa tu edad:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa tu peso:");
+            int peso = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa tu altura:");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa tu estado civil:");
+            var estadoCivil = Console.ReadLine();
+            Console.WriteLine("Ingresa tu provincia:");
+            var provincia = Console.ReadLine();
+            Console.WriteLine("Ingresa tu ciudad:");
+            var ciudad = Console.ReadLine();
+            DateTime fechaIngreso = DateTime.Now;
+
+            Persona namePerson;
+            namePerson = new Persona(); 
+               
+            Console.WriteLine("******************| PROGRAMA | ******************** ");
+            Console.WriteLine(namePerson.imprimeNombres(name1,name2,name3,name4));
+            Console.WriteLine(namePerson.imprimeDatos(edad, peso,altura));
+            Console.WriteLine(namePerson.imprimeEstado(estadoCivil, provincia, ciudad));
+            Console.WriteLine("Fecha Actual: " + fechaIngreso);
+            Console.ReadKey();
+        }    
+    }
+    class Persona
+    {
+       
+        public string imprimeNombres(string name1,string name2, string name3, string name4) {
+            return "Nombres: "+ name1 +" " + name2 + " " + name3 + " " + name4+".";
         }
+        public string imprimeDatos(int edad, int peso, double altura) {
+            return  "Tienes: " + edad +" años\n Tu peso es:" + peso +" Kg.\n Tu estatura es: " + altura + "cm." ;
+        }
+        public string imprimeEstado(string estadoCivil, string provincia, string ciudad) {
+            return "Estado civil: "+estadoCivil +"\n Provincia: "+provincia +"\n Ciudad: " + ciudad+".";
+        }
+
     }
 }
