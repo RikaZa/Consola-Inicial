05f88191382387300e236e5ed6cd11ab93b0026e
JRZR1723 <zajo.deox@gmail.com>
2021-06-18 01:13:00 -0500

    18.06.2021

---
+++ Fist_app/Fist_app/Persona.cs
@@ -0,0 +1,17 @@
+﻿namespace Fist_app
+{
+    class Persona
+    {
+       
+        public string imprimeNombres(string name1,string name2, string name3, string name4) {
+            return "Nombres: "+ name1 +" " + name2 + " " + name3 + " " + name4+".";
+        }
+        public string imprimeDatos(int edad, int peso, double altura) {
+            return  "Tienes: " + edad +" años.\nTu peso es:" + peso +" Kg.\nTu estatura es: " + altura + " cm." ;
+        }
+        public string imprimeEstado(string estadoCivil, string provincia, string ciudad) {
+            return "Estado civil: "+estadoCivil+".\n" +"Provincia: "+provincia+".\n" + "Ciudad: " + ciudad+".";
+        }
+
+    }
+}
--- Fist_app/Fist_app/Program.cs
+++ Fist_app/Fist_app/Program.cs
@@ -39,18 +39,4 @@ namespace Fist_app
             Console.ReadKey();
         }    
     }
-    class Persona
-    {
-       
-        public string imprimeNombres(string name1,string name2, string name3, string name4) {
-            return "Nombres: "+ name1 +" " + name2 + " " + name3 + " " + name4+".";
-        }
-        public string imprimeDatos(int edad, int peso, double altura) {
-            return  "Tienes: " + edad +" años.\nTu peso es:" + peso +" Kg.\nTu estatura es: " + altura + "cm." ;
-        }
-        public string imprimeEstado(string estadoCivil, string provincia, string ciudad) {
-            return "Estado civil: "+estadoCivil+".\n" +"Provincia: "+provincia+".\n" + "Ciudad: " + ciudad+".";
-        }
-
-    }
 }
